f480c7420997220b9b54048bed8b7c42a65197e9
Yali <byzhkc3@gmail.com>
2016-08-24 17:17:00 +0800

    创建vue分支

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 module.exports = {
+	devtool: 'eval-source-map',
 	entry: [__dirname +"/src/main.js",
 		__dirname +"/src/index.js"
 	],
 	output:{
 		path: __dirname + "/dist",//output dir
 		filename: "bundle.js" //file name
+	},
+	devServer:{
+		contentBase: '',  //静态资源的目录 相对路径,相对于当前路径 默认为当前config所在的目录
+		devtool: 'eval',
+		hot: true,        //自动刷新
+		inline: true,    
+		port: 8090        
 	},
 	module:{
 		loaders:[
@@ -13,5 +21,5 @@ module.exports = {
 				loader:"style!css"
 			}
 		]
-	},
+	}, 
 }
